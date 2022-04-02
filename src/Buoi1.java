d14e38ec4189311274842265e6c068b8a62d44fd
dang248 <lequocdangbku18@gmail.com>
2022-04-02 19:33:12 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,26 @@
+public class Buoi1 {
+    // method, function = phương thức
+    public static void main(String[] args) {
+        // 1 kiểu dữ liệu
+
+            // Có 2 nhóm
+                // Kiểu nguyên thủy (primitives): Không cung cấp chức năng xử lí dữ liêu
+                    // String (chuỗi), => "Nguyễn Văn Tèo"
+                    // char (ký tự),   => 'a'
+                    // int (số nguyên), double lớn hơn int
+                    // float (số thực),
+                    // boolean (kiểu đúng hoặc sai).
+
+                // Kiểu đối tượng (object): Các phương thức hỗ trợ xử lí dữ liệu
+                    // Tạo ra một đối tượng Person có thuộc tính name, age
+        // 2 biến
+        String name = "Lê Quốc Đăng";
+        int age = 20;
+        String favorite = "nằm chơi";
+        float height = 1.75f;
+        double phone = 23101010101d;
+        String address = "TPHCM";
+        String work = "athome";
+        boolean isMale = true;
+    }
+}
